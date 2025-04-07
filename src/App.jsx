e766f542f330eb3ede479cb054602f2405f9ddd8
Saymon Shoab <76821250+saymon-shoab@users.noreply.github.com>
2025-04-07 06:37:46 +0600

    feat: Create page and ui with functionality for navigate validation form submit edit and delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import FontGroups from "./page/FontGroups";
-import FontUploader from "./page/FontUploader";
+import FontList from "./page/FontList";
+import Navbars from './components/Navbars';
+import CreateFontGroup from "./page/CreateFontGroup";
 
 
 function App() {
 
 
   return (
-    <Router>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
-        <Link to="/" className="navbar-brand">Font App</Link>
-        <div className="navbar-nav">
-          <Link to="/" className="nav-link">Font List</Link>
-          <Link to="/create" className="nav-link">Create Group</Link>
-        </div>
-      </nav>
-      <Routes>
-        <Route path="/" element={<FontUploader/>} />
-        <Route path="/create" element={<FontGroups />} />
-      </Routes>
-    </Router>
+   
+<BrowserRouter>
+  <Navbars /> 
+  <Routes>
+    <Route path="/" element={<FontList />} />
+    <Route path="/fontgroup" element={<FontGroups />} />
+    <Route path="/create-font-group" element={<CreateFontGroup />} />
+  </Routes>
+</BrowserRouter>
   );
 }
 
---
+++ src/page/FontUploader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FontUploader = () => {
+  return (
+    <div>FontUploader</div>
+  )
+}
+
+export default FontUploader
